a1e47e6ace1ab8434cedd24b8797d960105a07d6
andrade-UX <mauricioandrade94@gmail.com>
2025-03-04 19:11:29 -0300

    A solução digital para o seu negócio

--- www.devpix.com.br/src/components/HOME.tsx
+++ www.devpix.com.br/src/components/HOME.tsx
@@ -3,17 +3,17 @@ import Image from 'next/image';
 export default function Homepage() {
     return (
         <main>
-            <div className="relative flex justify-center bg-white px-10 py-16">
+            <div className="relative flex justify-center bg-white px-10 py-16 ">
                 {/* Contêiner de fundo */}
                 <div className="absolute inset-0 flex">
                     {/* SVG Cinza */}
-                    <div className="w-2/3 relative z-10">
+                    <div className="w-2/3 relative z-10 ">
                         <Image
                             src="/vector1.svg"
                             alt="Fundo Cinza"
                             width={500}
                             height={300}
-                            className="w-full h-auto"
+                            className="w-full  h-[450px]"
                         />
                     </div>
                     {/* Contêiner azul ao fundo responsivo */}
@@ -22,11 +22,11 @@ export default function Homepage() {
 
                 {/* Texto e botões */}
                 <div className="relative z-10 max-w-lg">
-                    <h1 className="text-4xl font-bold mt-15 text-gray-900 leading-tight">
-                        O <span className="text-purple-600">visual</span> da sua empresa vai impactar na escolha do seu cliente
+                    <h1 className="text-4xl font-bold mt-16 text-gray-900 leading-tight">
+                        O <span className="text-blue-800">visual</span> da sua empresa vai impactar na escolha do seu cliente
                     </h1>
                     <p className="text-gray-700 mt-4">
-                        Comece com custo 0 e cancele quando quiser.
+                        Comece com custo R$0 e cancele quando quiser.
                     </p>
 
                     {/* Botões */}
@@ -54,21 +54,21 @@ export default function Homepage() {
                 <div class="flex justify-center gap-36">
 
                     <div class="flex flex-col items-center bg-[url('/background1.svg')] bg-no-repeat bg-center bg-cover w-40 h-40 pt-5">
-                        <img src="/icon-visibilidade.svg" alt="" class="mb-3"/>
-                        <h1 class='text-blue-800 font-bold text-2xl leading-tight'>+ Visibilidade</h1>
-                        <p  class="text-center max-w-lg">Maior exposição para sua empresa</p>
+                        <img src="/icon-visibilidade.svg" alt="" className="mb-3" />
+                        <h1 className='text-blue-800 font-bold text-2xl leading-tight'>+ Visibilidade</h1>
+                        <p className="text-center max-w-lg">Maior exposição para sua empresa</p>
                     </div>
 
                     <div class="flex flex-col items-center bg-[url('/background1.svg')] bg-no-repeat bg-center bg-cover w-40 h-40 pt-5">
-                        <img src="/icon-metricas.svg" alt="" class="mb-3"/>
+                        <img src="/icon-metricas.svg" alt="" class="mb-3" />
                         <h1 class='text-blue-800 font-bold text-2xl leading-tight'>Métricas</h1>
-                        <p  class="text-center max-w-lg">Monitore o trafego</p>
+                        <p class="text-center max-w-lg">Monitore o trafego</p>
                     </div>
 
                     <div class="flex flex-col items-center bg-[url('/background1.svg')] bg-no-repeat bg-center bg-cover w-40 h-40 pt-5">
-                        <img src="/icon-fidelizacao.svg" alt="" class="mb-3"/>
+                        <img src="/icon-fidelizacao.svg" alt="" class="mb-3" />
                         <h1 class='text-blue-800 font-bold text-2xl leading-tight'>Fidelidade</h1>
-                        <p  class="text-center max-w-lg">Maior busca pela sua marca</p>
+                        <p class="text-center max-w-lg">Maior busca pela sua marca</p>
                     </div>
                 </div>
 
@@ -78,6 +78,33 @@ export default function Homepage() {
                     </p>
                 </div>
             </div>
+
+
+            <div class="flex justify-center items-start w-4/5 mx-auto mt-20 gap-4">
+                <div class="flex justify-center items-center w-1/2">
+                    <img src="/mulher_de_ti.png" alt="Mulher de TI" class="w-96" />
+                </div>
+                <div class="w-1/2 flex flex-col items-start">
+                    <h1 class="text-4xl font-bold text-gray-900 leading-tight">
+                    A <span class="text-blue-800">solução digital</span> para o seu negócio.
+                    </h1>
+                    <p class="text-gray-700 mt-4 text-justify">
+                        Destaque-se com um site profissional feito sob medida para sua marca! A primeira impressão é decisiva, especialmente na internet. Sua empresa merece um site moderno, rápido e que transmita confiança desde o primeiro clique. Na DevPix, transformamos ideias em experiências digitais excepcionais, utilizando design limpo, funcional e totalmente personalizado.
+                    </p>
+
+                    <div class="mt-6 flex gap-4">
+                        <button class="bg-blue-900 text-white px-6 py-3 rounded-md font-semibold">
+                            Descubra o Devpix &rarr;
+                        </button>
+                        <button class="border-2 border-blue-900 px-6 py-3 rounded-md font-semibold">
+                            Solicitar contato &rarr;
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+
+
 
         </main>
     );
